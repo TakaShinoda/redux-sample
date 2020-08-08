e2adca33065d1ae16b073791b5f891916496e68e
Takahiro <tkhr_621@docomo.ne.jp>
2020-08-08 23:38:28 +0900

    App.jsを移動

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './Button'
+import { Message } from './Message'
+
+export const App = () => {
+  return (
+    <>
+      <h1>Redux</h1>
+      <Message />
+      <Button />
+    </>
+  )
+}
